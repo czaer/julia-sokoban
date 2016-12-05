dff0f5a41d03f2794e1db606bf109899748b4a72
Anton Vasick <avasick@uci.edu>
2016-12-04 22:54:37 -0800

    fixed more bugs + added stable marriage

--- gameState.jl
+++ gameState.jl
@@ -39,22 +39,27 @@ end
 #if a move is allowed, generates a new state after the move and returns that and true
 #otherwise returns the original state and false
 function move(direction::Char, state::State, board::Board)
+  #println("initial state: $state")
     @match direction begin
         'U' =>  begin
                     guyDest = [state.guy[1]-1, state.guy[2]]
                     pushBoxLoc = [guyDest[1]-1, guyDest[2]]
+                    #println("U")
                 end
         'D' => begin
                     guyDest = [state.guy[1]+1, state.guy[2]]
                     pushBoxLoc = [guyDest[1]+1, guyDest[2]]
+                    #println("D")
                 end
         'L' => begin
                     guyDest = [state.guy[1], state.guy[2]-1]
                     pushBoxLoc = [guyDest[1], guyDest[2]-1]
+                    #println("L")
                 end
         'R' => begin
                     guyDest = [state.guy[1], state.guy[2]+1]
                     pushBoxLoc = [guyDest[1], guyDest[2]+1]
+                    #println("R")
                 end
     end
 
@@ -63,22 +68,30 @@ function move(direction::Char, state::State, board::Board)
 
     if in(guyDest, board.walls)
         newState = state
+        #println("1 newstate: $newState")
     elseif in(guyDest, state.boxes)
         if in(pushBoxLoc, board.walls)
             newState = state
+            #println("2 newstate: $newState")
         elseif in(pushBoxLoc, state.boxes)
             newState = state
+            #println("3 newstate: $newState")
         else
             #is clear or switch
             newState = deepcopy(state)
+            #println("4 newstate: $newState")
             #newState.guy = guyDest
             setGuy(guyDest, newState, board)
             bxs = deepcopy(newState.boxes)
             push!(bxs, pushBoxLoc)
             #not sure which way the logic flows here
             #bxs.pop!(guyDest)
-            deleteat!(bxs, findin(bxs, guyDest))
+            #println(guyDest)
+            #println(findfirst(bxs, guyDest))
+            deleteat!(bxs, findfirst(bxs, guyDest))
+            #println("5 boxes: $bxs")
             setBoxes(bxs, newState, board )
+            #println("6 newstate: $newState")
             moveExecuted = true
         end
     else
@@ -94,8 +107,108 @@ function computeH!(state::State, board::Board)
     state.hVal = computeHInit(state.guy, state.boxes, board)
 end
 
+function generatePref(men::Array{Int64,1}, women::Array{Array{Int64,1},1})
+  distances = Dict()
+  i = 1
+  for s in women
+    distances[i] = (abs(men[1] - s[1])+abs(men[2] - s[2]))
+    i += 1
+  end
+  ret = Int64[]
+  for (key, value) in distances
+    if length(ret) == 0
+      push!(ret, key)
+      continue
+    end
+    insterted = false
+    for x in ret
+      if value < distances[x]
+        splice!(ret, findfirst(ret, x), [key, x])
+        insterted = true
+        break
+      end
+    end
+    if !insterted
+      push!(ret, key)
+    end
+  end
+  return ret
+end
+
+function computeHInit(guy::Array{Int64,1}, boxes::Array{Array{Int64,1},1}, board::Board)
+  switches = collect(board.switches)
+  freeBoxes = trues(length(boxes))
+  freeSwitches = trues(length(switches))
+  prefBoxes = Array{Array{Int64}, 1}(0)
+  prefSwitches = Array{Array{Int64}, 1}(0)
+  pairs = Array{Int64}(0)
+  #println("stupid boxes")
+  for b in boxes
+    push!(prefBoxes, generatePref(b, switches))
+    push!(pairs, 0)
+  end
+  #println("1")
+  for s in switches
+    push!(prefSwitches, generatePref(s, boxes))
+  end
+#  println("2")
+  while true in freeBoxes
+    matched = false
+    x = findfirst(freeBoxes, true)
+  #  println("3")
+  #  println("x: $x, prefBoxes: $prefBoxes")
+    for w in prefBoxes[x]
+      #println("4")
+     if freeSwitches[w]
+       freeSwitches[w] = false
+       freeBoxes[x] = false
+       pairs[x] = w
+      # println("5")
+       break
+     else
+      # println("6")
+      # println("w: $w")
+       old = findfirst(pairs, w)
+      # println("pairs: $pairs")
+      # println("7")
+       for m in prefSwitches[w]
+         #println("8")
+         if m == x
+           #println("9")
+           #println("old: $old")
+           freeBoxes[x] = false
+           freeBoxes[old] = true
+           pairs[old] = 0
+           pairs[x] = w
+           matched = true
+           #println("10")
+           break
+         end
+         if m == old
+           break
+         end
+         if matched
+           break
+         end
+       end
+       if matched
+         break
+       end
+     end
+   end
+ end
+ #println("pairs at end: $pairs")
+ hTotal = 0
+ i = 1
+ while i <= length(pairs)
+   hTotal += (abs(boxes[i][1] - switches[pairs[i]][1]) + abs(boxes[i][2] - switches[pairs[i]][2]))
+   i+=1
+ end
+ return hTotal
+end
+
 # assuming length(boxes) == length(switches)
-function computeHInit(guy::Array{Int64,1},boxes::Array{Array{Int64,1},1}, board::Board)
+function computeHInitOld(guy::Array{Int64,1},boxes::Array{Array{Int64,1},1}, board::Board)
     #iff gameState = goal then hVal= 0
     #if guy can't move, return maxint
     #too slow version. foreach switch, pathfind the nearest box. add up distances
